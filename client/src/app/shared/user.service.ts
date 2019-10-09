d866b0d709332c4323f773374c9592dfb2633aa0
Rashika Koul <36055710+rashikak295@users.noreply.github.com>
2019-10-08 23:39:41 -0400

    Added client

---
+++ client/src/app/shared/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  selectedUser: User = {
+    fullname: '',
+    email: '',
+    password: ''
+  };
+
+  constructor(private http: HttpClient) { }
+
+  postUser(user: User) {
+    return this.http.post(environment.apiBaseUrl + '/register', user);
+  }
+}
---
+++ client/src/app/user/sign-up/sign-up.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/user.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+  providers: [UserService]
+})
+export class SignUpComponent implements OnInit {
+  // tslint:disable-next-line:max-line-length
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  showSuccessMessage: boolean;
+  serverErrorMessages: string;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.userService.postUser(form.value).subscribe(
+      res => {
+        this.showSuccessMessage = true;
+        setTimeout(() => this.showSuccessMessage = false, 3000);
+        this.resetForm(form);
+      },
+      err => {
+        if (err.status === 422) {
+          this.serverErrorMessages = err.console.error.join('<br/>');
+        } else {
+          this.serverErrorMessages = 'Oops! Looks like something went wrong!';
+        }
+      }
+    );
+  }
+
+  resetForm(form: NgForm) {
+    this.userService.selectedUser = {
+      fullname: '',
+      email: '',
+      password: ''
+    };
+    form.resetForm();
+    this.serverErrorMessages = '';
+  }
+
+}
